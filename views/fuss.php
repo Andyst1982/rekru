c0821aa85ddd901e9f4a1e7fa07f41d15b4b0a9d
Andyst1982 <andreasstuck@yahoo.de>
2024-04-16 16:33:50 +0200

    Fuss

--- views/fuss.php
+++ views/fuss.php
@@ -1,4 +1,11 @@
-
-
+<style>
+p {
+  font-size: 0.5em; /* responsative Schrift größe */
+  position: absolute; /* absolute position */
+  bottom: 10px;/* von unten */
+  right: 10px;/* von rechts */
+}
+</style>
+<p>&copy; 2024 Andy1982</p>
 </body>
 </html>
