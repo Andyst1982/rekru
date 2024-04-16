f8f3e149a94910b45f9ca15f7df981d58bc6091a
Andyst1982 <andreasstuck@yahoo.de>
2024-04-16 12:15:18 +0200

    Kopfzeile index korrektur

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
-<?php
-$titel="Rekrutierungs System";
-include"views/kopf.php";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/inc/css/style.css">
+    <title>Rekrutierungs System</title>
+</head>
+<body>
+    <h1>Rekrutierungs System</h1>
 <ul>
         <li><a href="views/jobangeboteAnzeigen.php">Jobangebote anzeigen</a></li>
         <li><a href="views/jobangebotErstellen.php">Jobangebot erstellen</a></li>
